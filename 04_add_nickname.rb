ac8cfbe6a3f7081c9ce592ec2dfe3a7e422890c2
Rick Walker <walkrick@yahoo.com>
2014-06-19 14:10:01 -0600

    print nickname

--- 04_add_nickname.rb
+++ 04_add_nickname.rb
@@ -1,3 +1,5 @@
 require_relative "person"
 
 # Add a nickname to the BJORN_BORG hash and print it out. Don't edit the person.rb file!
+BJORN_BORG.store("nickname", "BJ")
+print BJORN_BORG["nickname"]
