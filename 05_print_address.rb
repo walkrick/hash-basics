f200c92673ee9fe43ba85ff9b256ba0a69a2928b
Rick Walker <walkrick@yahoo.com>
2014-06-19 14:43:02 -0600

    print address

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,7 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+
+print "This is Bjorn Borg's address: #{BJORN_BORG["address"]["street"]}, #{BJORN_BORG["address"]["city"]}, #{BJORN_BORG["address"]["state"]}, #{BJORN_BORG["address"]["zip_code"]}
+"
+
