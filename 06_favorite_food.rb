dddda0f1f2bb7225ec2af29e7102ad6ec683f6a8
Rick Walker <walkrick@yahoo.com>
2014-06-20 11:37:19 -0600

    fovorite food

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,11 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+
+print "Bjorn's favorite foods are #{BJORN_BORG["favorite_foods"][0]}, #{BJORN_BORG["favorite_foods"][1]} and #{BJORN_BORG["favorite_foods"][2]}."
+
+
+
+
+
